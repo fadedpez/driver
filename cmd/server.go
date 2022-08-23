501457280d8d939c8468b8cba0a693f046361c00
frank <fvl7997@gmail.com>
2022-08-22 21:36:43 -0400

    adding boilerplate

---
+++ cmd/server.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+var serverCommand = &cobra.Command{
+	Use:   "server",
+	Short: "Run the gRPC server",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Print("starting server")
+		address := "localhost:5000"
+		lis, err := net.Listen("tcp", address)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		s := grpc.NewServer()
+
+		log.Printf("server listening at %v", lis.Addr())
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCommand.AddCommand(serverCommand)
+}
