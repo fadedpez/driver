b613d7804c0dcd1d8e6da5266c1bbe07c01dfb83
frank <fvl7997@gmail.com>
2022-08-23 16:18:35 -0400

    created interface, mock, and simple entity

---
+++ internal/entities/driver.go
@@ -0,0 +1,12 @@
+package entities
+
+import "time"
+
+type Driver struct {
+	FirstName   string `dynamodbav:"first_name"`
+	LastName    string `dynamodbav:"last_name"`
+	Number      string `dynamodbav:"driver_number"`
+	Nationality string `dynamodbav:"driver_nationality"`
+	dob         *time.Time
+	ID          string `dynamodbav:"id"`
+}
