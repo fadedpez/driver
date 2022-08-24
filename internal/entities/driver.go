35bcf2d4a5d56af05e5255438d54a1884f8649e5
frank <fvl7997@gmail.com>
2022-08-23 20:24:26 -0400

    added createdriver to alpha and created associated tests

--- internal/entities/driver.go
+++ internal/entities/driver.go
@@ -1,12 +1,6 @@
 package entities
 
-import "time"
-
 type Driver struct {
-	FirstName   string `dynamodbav:"first_name"`
-	LastName    string `dynamodbav:"last_name"`
-	Number      string `dynamodbav:"driver_number"`
-	Nationality string `dynamodbav:"driver_nationality"`
-	dob         *time.Time
-	ID          string `dynamodbav:"id"`
+	Name string `dynamodbav:"name"`
+	ID   string `dynamodbav:"name"`
 }
--- internal/handlers/alpha_test.go
+++ internal/handlers/alpha_test.go
@@ -1,8 +1,13 @@
 package handlers
 
 import (
+	"context"
 	"errors"
 	"testing"
+
+	"github.com/fadedpez/driver/internal/entities"
+	driverapialpha "github.com/fadedpez/driver/protos"
+	"github.com/stretchr/testify/mock"
 
 	"github.com/stretchr/testify/assert"
 
@@ -38,5 +43,69 @@ func TestNewAlpha(t *testing.T) {
 		assert.NotNil(t, err)
 		assert.Equal(t, expError, err)
 
+	})
+}
+
+func TestAlpha_CreateDriver(t *testing.T) {
+	t.Run("it calls the driver repo correctly", func(t *testing.T) {
+		handler := setupFixture()
+		m := handler.driverRepo.(*driver.MockRepo)
+
+		expDriver := &entities.Driver{
+			Name: "stan",
+		}
+
+		retDriver := &entities.Driver{
+			Name: "stan",
+			ID:   "0",
+		}
+
+		m.On("CreateDriver", expDriver).Return(retDriver, nil)
+
+		actual, err := handler.CreateDriver(context.Background(), &driverapialpha.CreateDriverRequest{
+			Name: "stan",
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, actual)
+		assert.Equal(t, &driverapialpha.CreateDriverResponse{
+			Driver: &driverapialpha.Driver{
+				Name: retDriver.Name,
+				Id:   retDriver.ID,
+			},
+		}, actual)
+		m.AssertExpectations(t)
+	})
+
+	t.Run("it returns an error when the driverRepo errors", func(t *testing.T) {
+		handler := setupFixture()
+		m := handler.driverRepo.(*driver.MockRepo)
+
+		expErr := errors.New("mock driver create failed")
+
+		m.On("CreateDriver", mock.Anything).Return(nil, expErr)
+
+		actual, err := handler.CreateDriver(context.Background(), &driverapialpha.CreateDriverRequest{
+			Name: "stan",
+		})
+
+		assert.Nil(t, actual)
+		assert.NotNil(t, err)
+		assert.Equal(t, expErr, err)
+		m.AssertExpectations(t)
+	})
+
+	t.Run("it requires a name", func(t *testing.T) {
+		handler := setupFixture()
+
+		expErr := errors.New("name cannot be empty.")
+
+		actual, err := handler.CreateDriver(context.Background(), &driverapialpha.CreateDriverRequest{
+			Name: "",
+		})
+
+		assert.Nil(t, actual)
+		assert.NotNil(t, err)
+		assert.Equal(t, expErr, err)
 	})
 }
